e30abb0b1c2a018d208cea31ff8a374a8363d096
saito_taiki <neural.neuman921@gmail.com>
2017-07-10 03:51:56 +0900

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :users
-  root to: 'home#index'
+  root to: 'recipes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
